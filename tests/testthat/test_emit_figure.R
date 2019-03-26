083cd8049f78fd58736cd462d25b1f976d0d6de7
bshepp <bshepp@umich.edu>
2019-03-26 12:09:41 -0400

    Accomodates R version 3.4

--- tests/testthat/test_emit_figure.R
+++ tests/testthat/test_emit_figure.R
@@ -12,7 +12,7 @@ test_that("emits .png to specified directory", {
   data <- generate_data_all_cols()
   spek <- generate_full_spek()
   summary <- list("benchmark_ten"=50)
-  figure_path <- tempdir(check=TRUE)
+  figure_path <- tempdir()
   emit_figure(data, spek, summary, figure_path)
   expect_true(file.exists(file.path(figure_path, "fraisty_summary.png")))
   unlink(file.path(figure_path, "fraisty_summary.png"))
