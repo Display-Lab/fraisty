9d4008b1432aa7aa3fd193508036338e84df86ea
bshepp <bshepp@umich.edu>
2019-03-26 10:52:15 -0400

    Visualizes performance based summaries.

--- tests/testthat/test_figure_summary.R
+++ tests/testthat/test_figure_summary.R
@@ -4,6 +4,7 @@ library('tibble')
 test_that("Figure summary takes in data and column name", {
   data <- tibble('ids'=1:10, 'performance'=seq(1, 100, by=10))
   column_name <- "performance"
-  result <- figure_summary(data, column_name)
+  summary <- list("benchmark_ten"=50)
+  result <- figure_summary(data, column_name, summary)
   expect_s3_class(result, "ggplot")
 })
